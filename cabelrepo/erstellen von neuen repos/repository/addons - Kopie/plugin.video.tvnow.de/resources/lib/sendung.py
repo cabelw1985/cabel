398b5691f24ed330559c5009f78e5a92c6e4078e
cabelw1985 <52887057+cabelw1985@users.noreply.github.com>
2021-12-06 17:59:19 +0100

    asdf

--- cabelrepo/erstellen von neuen repos/repository/addons - Kopie/plugin.video.tvnow.de/resources/lib/sendung.py
+++
@@ -1,7 +0,0 @@
-class Sendung():
-    def __init__(self, sid, title, station, tGroup, itemType):
-        self.sid = sid
-        self.title = title
-        self.station = station
-        self.tGroup = tGroup
-        self.itemType = itemType
